a85bc93b1cddcb4b176e8b42c088b96604665fa9
Jasmin <s170720@student.dtu.dk>
2019-11-02 12:49:45 +0100

    v1

--- src/main/java/Forbindelse.java
+++ src/main/java/Forbindelse.java
@@ -1,30 +1,31 @@
 import java.sql.*;
 
-
 public class Forbindelse {
 
     public static void main(String[] args) { //psvm
         Forbindelse forbindelse = new Forbindelse();
-        forbindelse.updateConnection("192.168.239.24","root","groot");
+        // forbindelse.updateConnection("192.168.239.24","root","groot");
+        forbindelse.searchUser( "0");
 
     }
 
     private Connection connection;
     private Statement stmt;
     private String url, userName, password;
+    private ResultSet userset,appointmentset;
+
 
     public void updateConnection(String newUrl, String newUsername,String newPassword) {
 
         url = "jdbc:mysql://"+newUrl+":3306/?user="+newUsername;
         userName = newUsername;
-        password=newPassword;
+        password = newPassword;
 
         try {
             connection=DriverManager.getConnection(url,userName,password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     public Forbindelse() {
@@ -40,18 +41,28 @@ public class Forbindelse {
             if (connection != null) {
                 System.out.println("forbindelse til databasen");
             }
+
+
         } catch (Exception e) {
             System.out.println("forbindelse undtagelse: " + e.getMessage());
             e.printStackTrace();
         }
 
+
+
+
     }
-    public void searchUser(){
+    public void searchUser(String cpr){
+
+        try {
+            userset = stmt.executeQuery("SELECT * FROM sund.person where cpr="+cpr+";");
+            int output = userset.getInt("cpr");
+            System.out.println("bruger fundet med tallet:"+output);
 
 
-
-
-
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
